0817f925293942a4e1df54fbb5cfaab9736ee7c1
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-06-05 02:38:45 -0400

    Added methods for registering function footprints to NameResolver

--- Horizon/Parsing/NameResolver.cs
+++ Horizon/Parsing/NameResolver.cs
@@ -7,4 +7,30 @@ internal class NameResolver
     {
         functionFootprints = new();
     }
+
+    public void RegisterFunctions(IEnumerable<Footprint> funcFootprints)
+    {
+        foreach (var footprint in funcFootprints)
+        {
+            RegisterFunction(footprint);
+        }
+    }
+
+    public void RegisterFunction(Footprint funcFootprint)
+    {
+        if (!functionFootprints.ContainsKey(funcFootprint.Name))
+            AddFunctionName(funcFootprint.Name);
+
+        AddFootprint(funcFootprint);
+    }
+
+    private void AddFootprint(Footprint funcFootprint)
+    {
+        functionFootprints[funcFootprint.Name].Add(funcFootprint);
+    }
+
+    private void AddFunctionName(string name)
+    {
+        functionFootprints.Add(name, new());
+    }
 }
