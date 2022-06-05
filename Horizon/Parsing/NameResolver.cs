ae251b7dab9038f7ea3e80bf4d63e7cd6bed8d92
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-06-05 02:38:25 -0400

    Added NameResolver

---
+++ Horizon/Parsing/NameResolver.cs
@@ -0,0 +1,10 @@
+﻿namespace Horizon.Parsing;
+internal class NameResolver
+{
+    private Dictionary<string, List<Footprint>> functionFootprints;
+
+    public NameResolver()
+    {
+        functionFootprints = new();
+    }
+}
