cfb1f980325e62c0593cdfd257e845a9f2eafc28
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-06-05 23:06:34 -0400

    Updated Horizon.Console to read source from file

--- Horizon.Console/Program.cs
+++ Horizon.Console/Program.cs
@@ -5,6 +5,6 @@ var log = new Logger(new ConsoleLogDestination(LogLevel.Debug));
 
 var compiler = new HorizonCompiler(log);
 
-string source = "123\n456 789.34\n\"Hello, World!\"\n17 + 4.3 * .5";
+string source = File.ReadAllText("testSource.hz");
 
 compiler.Compile(source);
